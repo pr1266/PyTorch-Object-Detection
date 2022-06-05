66cf4da7cc3f237415a593fcc47619fa67d101d2
pourya pooryeganeh <pour.pourya1999@gmail.com>
2022-06-05 19:20:36 +0430

    'box2'

--- intersection_over_union.py
+++ intersection_over_union.py
@@ -8,8 +8,16 @@ print(device)
 
 def intersection_over_union(boxes_preds, boxes_labels):
     #! inja box_preds 4 ta value dare: x1, y1, x2, y2
+    #TODO inja baraye box1
     box1_x1 = boxes_preds[..., 0:1]
     box1_y1 = boxes_preds[..., 1:2]
 
     box1_x2 = boxes_preds[..., 2:3]
     box1_y2 = boxes_preds[..., 3:4]
+
+    #TODO inja baraye box2
+    box2_x1 = boxes_preds[..., 0:1]
+    box2_y1 = boxes_preds[..., 1:2]
+
+    box2_x2 = boxes_preds[..., 2:3]
+    box2_y2 = boxes_preds[..., 3:4]
