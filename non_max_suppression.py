8a14dc6338a12197bfb77024dfeca30b072a3075
pourya pooryeganeh <pour.pourya1999@gmail.com>
2022-06-05 19:39:45 +0430

    'nms_hint'

--- non_max_suppression.py
+++ non_max_suppression.py
@@ -1,5 +1,8 @@
 
 #! inja mikhaim behtarin bounding box ro peida konim:
+#? hint: age iou bein e 2 ta bbox az threshold bishtar bashe,
+#? farz mikonim ke 2 ta bbox daran be ye object eshare mikonan
+#? pass ooni ke confidence bishtari dare ro negah midarim
 import torch
 from intersection_over_union import intersection_over_union
 
