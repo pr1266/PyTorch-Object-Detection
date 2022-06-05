3b4faf8c6115159cac3c83322ec9374c611e70c2
pourya pooryeganeh <pour.pourya1999@gmail.com>
2022-06-05 19:28:56 +0430

    'box_formats'

--- intersection_over_union.py
+++ intersection_over_union.py
@@ -6,21 +6,35 @@ import matplotlib.pyplot as plt
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(device)
 
-def intersection_over_union(boxes_preds, boxes_labels, box_format=''):
-    #! inja box_preds 4 ta value dare: x1, y1, x2, y2
-    #TODO inja baraye box1
-    box1_x1 = boxes_preds[..., 0:1]
-    box1_y1 = boxes_preds[..., 1:2]
+def intersection_over_union(boxes_preds, boxes_labels, box_format='midpoint'):
+    
+    #! box format haro joda mikonim:
+    if box_format == 'midpoint':
+        box1_x1 = boxes_preds[..., 0:1] - boxes_preds[..., 2:3] / 2
+        box1_y1 = boxes_preds[..., 1:2] - boxes_preds[..., 3:4] / 2
+        box1_x2 = boxes_preds[..., 0:1] + boxes_preds[..., 2:3] / 2
+        box1_y2 = boxes_preds[..., 1:2] + boxes_preds[..., 3:4] / 2
+        box2_x1 = boxes_labels[..., 0:1] - boxes_labels[..., 2:3] / 2
+        box2_y1 = boxes_labels[..., 1:2] - boxes_labels[..., 3:4] / 2
+        box2_x2 = boxes_labels[..., 0:1] + boxes_labels[..., 2:3] / 2
+        box2_y2 = boxes_labels[..., 1:2] + boxes_labels[..., 3:4] / 2
 
-    box1_x2 = boxes_preds[..., 2:3]
-    box1_y2 = boxes_preds[..., 3:4]
+    elif box_format == 'corners':
+    
+        #! inja box_preds 4 ta value dare: x1, y1, x2, y2
+        #TODO inja baraye box1
+        box1_x1 = boxes_preds[..., 0:1]
+        box1_y1 = boxes_preds[..., 1:2]
 
-    #TODO inja baraye box2
-    box2_x1 = boxes_preds[..., 0:1]
-    box2_y1 = boxes_preds[..., 1:2]
+        box1_x2 = boxes_preds[..., 2:3]
+        box1_y2 = boxes_preds[..., 3:4]
 
-    box2_x2 = boxes_preds[..., 2:3]
-    box2_y2 = boxes_preds[..., 3:4]
+        #TODO inja baraye box2
+        box2_x1 = boxes_preds[..., 0:1]
+        box2_y1 = boxes_preds[..., 1:2]
+
+        box2_x2 = boxes_preds[..., 2:3]
+        box2_y2 = boxes_preds[..., 3:4]
 
     #! inja max haro dar miarim:
     x1 = torch.max(box1_x1, box2_x1)
