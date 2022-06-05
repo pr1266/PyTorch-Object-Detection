e564f7f93eebecf020561888951b6f7c5c34e43d
pourya pooryeganeh <pour.pourya1999@gmail.com>
2022-06-05 19:48:50 +0430

    'nms_return_value'

--- non_max_suppression.py
+++ non_max_suppression.py
@@ -20,4 +20,24 @@ def nms(bboxes, iou_threshold, threshold, box_format='corners'):
     #TODO inja filter mikonim va ye seri ro bar asas e
     #TODO confidence level hazf mikonim:
     bboxes = [box for box in bboxes if box[1] > threshold]
-    
+    bboxes = sorted(bboxes, key=lambda x: x[1], reverse=True)
+    bboxes_after_nms = []
+
+    while bboxes:
+        chosen_box = bboxes.pop(0)
+
+        bboxes = [
+            box
+            for box in bboxes
+            if box[0] != chosen_box[0]
+            or intersection_over_union(
+                torch.tensor(chosen_box[2:]),
+                torch.tensor(box[2:]),
+                box_format=box_format,
+            )
+            < iou_threshold
+        ]
+
+        bboxes_after_nms.append(chosen_box)
+
+    return bboxes_after_nms
