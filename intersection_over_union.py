a401459b8c02ee81b0b0317e95beb85fbecf12f2
pourya pooryeganeh <pour.pourya1999@gmail.com>
2022-06-05 19:26:19 +0430

    'return_value'

--- intersection_over_union.py
+++ intersection_over_union.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(device)
 
-def intersection_over_union(boxes_preds, boxes_labels):
+def intersection_over_union(boxes_preds, boxes_labels, box_format=''):
     #! inja box_preds 4 ta value dare: x1, y1, x2, y2
     #TODO inja baraye box1
     box1_x1 = boxes_preds[..., 0:1]
@@ -21,3 +21,16 @@ def intersection_over_union(boxes_preds, boxes_labels):
 
     box2_x2 = boxes_preds[..., 2:3]
     box2_y2 = boxes_preds[..., 3:4]
+
+    #! inja max haro dar miarim:
+    x1 = torch.max(box1_x1, box2_x1)
+    y1 = torch.max(box1_y1, box2_y1)
+    x2 = torch.min(box1_x2, box2_x2)
+    y2 = torch.min(box1_y2, box2_y2)
+
+    intersection = (x2 - x1).clamp(0) * (y2 - y1).clamp(0)
+
+    box1_area = abs((box1_x2 - box1_x1) * (box1_y1 - box1_y2))
+    box2_area = abs((box2_x2 - box2_x1) * (box2_y1 - box2_y2))
+
+    return intersection / (box1_area + box2_area - intersection + 1e-6)
