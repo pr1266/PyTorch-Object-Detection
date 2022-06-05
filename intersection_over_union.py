cca5b74713b45083ded8249e1b372291a3b3930f
pourya pooryeganeh <pour.pourya1999@gmail.com>
2022-06-05 19:15:54 +0430

    'iou''

--- intersection_over_union.py
+++ intersection_over_union.py
@@ -1,6 +1,15 @@
 import torch
 import torch.nn as nn
-
+import numpy as np
+import matplotlib.pyplot as plt
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(device)
+
+def intersection_over_union(boxes_preds, boxes_labels):
+    #! inja box_preds 4 ta value dare: x1, y1, x2, y2
+    box1_x1 = boxes_preds[..., 0:1]
+    box1_y1 = boxes_preds[..., 1:2]
+
+    box1_x2 = boxes_preds[..., 2:3]
+    box1_y2 = boxes_preds[..., 3:4]
