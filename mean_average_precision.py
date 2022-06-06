665e2c478628d68bdb6fe149b164e1d706dab0ad
pourya pooryeganeh <pour.pourya1999@gmail.com>
2022-06-06 13:20:15 +0430

    'collect_boxes'

--- mean_average_precision.py
+++ mean_average_precision.py
@@ -5,4 +5,21 @@ import numpy as np
 
 def mean_average_precision(pred_boxes, true_boxes, iou_th=0.5, box_format='corners', num_classes=20):
 
-    
+    #! pred box list e mesle hint haye code haye ghabli
+    average_precisions = []
+    epsilon = 1e-6
+
+    #! baraye har class bayad hesab konim:
+    for c in range(num_classes):
+        detections = []
+        ground_truths = []
+
+        for detection in pred_boxes:
+            if detection[1] == c:
+                detections.append(detection)
+
+        for true_box in true_boxes:
+            if true_box[1] == c:
+                ground_truths.append(true_box)
+
+        amount_bboxes = Counter([gt[0] for gt in ground_truths])
