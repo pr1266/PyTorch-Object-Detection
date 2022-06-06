800180763957c8b07c3f1f7692651ff0cf8ab81c
pourya pooryeganeh <pour.pourya1999@gmail.com>
2022-06-06 13:16:33 +0430

    'map_func'

--- mean_average_precision.py
+++ mean_average_precision.py
@@ -1,1 +1,8 @@
 import torch
+from collections import Counter
+from intersection_over_union import intersection_over_union
+import numpy as np
+
+def mean_average_precision(pred_boxes, true_boxes, iou_th=0.5, box_format='corners', num_classes=20):
+
+    
