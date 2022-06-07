36479e1b1375732c859c21ea139ec20638a4d266
pourya pooryeganeh <pour.pourya1999@gmail.com>
2022-06-07 12:07:18 +0430

    'get_boxes'

--- yolo/dataset.py
+++ yolo/dataset.py
@@ -5,5 +5,26 @@ from PIL import Image
 
 class VOCDataset(torch.utils.DataSet):
 
-    def __init__(self, csv_file):
-        pass
+    def __init__(self, csv_file, img_dir, label_dir, S=7, B=2, C=20, transform=None):
+        self.annotations = pd.read_csv(csv_file)
+        self.img_dir = img_dir
+        self.label_dir = label_dir
+        self.transform = transform
+        self.S = S
+        self.B = B
+        self.C = C
+
+    def __len__(self):
+        return len(self.annotations)
+
+    def __getitem__(self):
+        label_path = os.path.join(self.label_dir, self.annotations.iloc[index, 1])
+        boxes = []
+        with open(label_path) as f:
+            for label in f.readlines():
+                class_label, x, y, width, height = [
+                    float(x) if float(x) != int(float(x)) else int(x)
+                    for x in label.replace("\n", "").split()
+                ]
+
+                boxes.append([class_label, x, y, width, height])
