94a6bd8361cc5ec953d3203b490f7186660043d2
pourya pooryeganeh <pour.pourya1999@gmail.com>
2022-06-07 12:17:56 +0430

    'dataset_finish'

--- yolo/dataset.py
+++ yolo/dataset.py
@@ -17,7 +17,7 @@ class VOCDataset(torch.utils.DataSet):
     def __len__(self):
         return len(self.annotations)
 
-    def __getitem__(self):
+    def __getitem__(self, index):
         label_path = os.path.join(self.label_dir, self.annotations.iloc[index, 1])
         boxes = []
         with open(label_path) as f:
@@ -28,3 +28,29 @@ class VOCDataset(torch.utils.DataSet):
                 ]
 
                 boxes.append([class_label, x, y, width, height])
+
+        img_path = os.path.join(self.img_dir, self.annotations.iloc[index, 0])
+        image = Image.open(img_path)
+        boxes = torch.tensor(boxes)
+
+        if self.transform:
+            image, boxes = self.transform(image, boxes)
+        
+        label_matrix = torch.zeros((self.S, self.S, self.C + 5 * self.B))
+        for box in boxes:
+            class_label, x, y, width, height = box.tolist()
+            class_label = int(class_label)
+            i, j = int(self.S * y), int(self.S * x)
+            x_cell, y_cell = self.S * x - j, self.S * y - i
+            width_cell, height_cell = (
+                width * self.S,
+                height * self.S
+            )
+
+            if label_matrix[i, j, 20] == 0:
+                label_matrix[i, j, 20] = 1
+                box_coordinates = torch.tensor(
+                    [x_cell, y_cell, width_cell, height_cell]
+                )
+                label_matrix[i, j, 21:25] = box_coordinates
+                label_matrix[i, j, class_label] = 1
