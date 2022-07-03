b814f9eabdab955e086e50d40fd19d5c8810af76
pourya <pourya_1999@yahoo.com>
2022-07-04 03:05:54 +0430

    'clean_up'

--- ssd/ssd.py
+++ ssd/ssd.py
@@ -4,3 +4,5 @@ import torch.nn as nn
 #! kolan ssd miad be ezaye har class, tooye tasvir 8732 ta bbox ba
 #! size haye mokhtalef peyda mikone baad miad ba nms in bbox haro delete mikone
 #! 2 ta architecture dare ssd: 300 va 512 ke neshoon dahande size input image an
+#! haji hal nadashtam dige:/
+#! ishala implementesh mikonam:))))))
